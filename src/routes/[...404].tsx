45162c996309a8912ef4440797a1b1fdb491f468
Matthias Reis <mr@mreis.me>
2026-04-13 08:03:34 +0200

    fix: not found route sends 404

--- src/routes/[...404].tsx
+++ src/routes/[...404].tsx
@@ -1,8 +1,10 @@
 import { A } from "@solidjs/router";
+import { HttpStatusCode } from "@solidjs/start";
 
 export default function NotFound() {
   return (
     <main class="text-center mx-auto text-gray-700 p-4">
+      <HttpStatusCode code={404} />
       <h1 class="max-6-xs text-6xl text-sky-700 font-thin uppercase my-16">Not Found</h1>
       <p class="mt-8">
         Visit{" "}
